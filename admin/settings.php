d92e81722f3d8a9faede54468d3e303219827328
Shankaran Patel <shankaranpatel@gmail.com>
2014-01-06 17:57:34 +0530

    Added Custom settings for project

---
+++ admin/settings.php
@@ -0,0 +1,48 @@
+<?php
+$postCategoryArgs = array(
+    'type' => 'post',
+    'child_of' => 0,
+    'parent' => '',
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'hide_empty' => 0,
+    'hierarchical' => 1,
+    'exclude' => '',
+    'include' => '',
+    'number' => '',
+    'taxonomy' => 'category',
+    'pad_counts' => false
+);
+$postCategories = get_categories($postCategoryArgs);
+$spCustomSettings = get_option('spCustomSettings');
+
+?>
+<div class="wrap">
+    <h2>Kindred Settings</h2>
+    <?php settings_errors('', true,true);?>
+    <form action="options.php" method="POST">
+        <?php settings_fields('wp_sp_settings_group'); ?>
+        <table class="form-table">
+            <tbody>
+            <tr>
+                <th valign="top" scope="row">Post Slider Category</th>
+                <td>
+                    <select name="spCustomSettings[postSliderCategory]">
+                        <option value="--Please select category--"></option>
+                        <?php foreach ($postCategories as $category) : ?>
+                            <option
+                                <?php if ($spCustomSettings['postSliderCategory'] == $category->cat_ID) : ?> selected <?php endif ?>
+                                value="<?php echo $category->cat_ID ?>"><?php echo $category->name; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+        <p class="submit">
+            <input type="submit" value="Save Changes" class="button button-primary" id="submit"
+                                 name="submit">
+        </p>
+    </form>
+</div>
---
+++ custom-settings.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Custom Settings
+Plugin URI: https://github.com/shankaranbonzer/wordpress
+Description: Provide Custom settings for kindred
+Version: 1.1
+Author: Shankaran Patel
+Author URI: http://bonzertech.com
+License: GPLv2
+*/
+
+/**
+ *
+ * @package Kindred
+ * @author Shankaran Patel
+ * @since 1.3
+ * @version 1.5
+ */
+
+/*
+ *
+ */
+
+add_action( 'init', 'sp_github_plugin_updater' );
+
+function sp_github_plugin_updater() {
+
+    include_once 'updater.php';
+
+    define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+    if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+        $config = array(
+            'slug' => plugin_basename( __FILE__ ),
+            'proper_folder_name' => 'custom-settings',
+            'api_url' => 'https://api.github.com/repos/shankaranbonzer/wordpress', // the github API url of your github repo
+            'raw_url' => 'https://raw.github.com/shankaranbonzer/wordpress/master', // the github raw url of your github repo
+            'github_url' => 'https://github.com/shankaranbonzer/wordpress', // the github url of your github repo
+            'zip_url' => 'https://github.com/shankaranbonzer/wordpress/zipball/master', // the zip url of the github repo
+            'sslverify' => true,
+            'requires' => '3.0',
+            'tested' => '3.3',
+            'readme' => 'README.md',
+            'access_token' => '',
+        );
+
+        new WP_GitHub_Updater( $config );
+
+    }
+
+}
+
+// admin page
+function sp_custom_settings_form()
+{
+    include('admin/settings.php');
+}
+
+// admin menu
+function sp_custom_settings_menu()
+{
+    add_options_page('kindredCustomSetting', ' Kindred Settings', 'manage_options', 'kindred-settings', 'sp_custom_settings_form');
+}
+
+add_action('admin_menu', 'sp_custom_settings_menu');
+
+
+// register setting
+function sp_custom_settings_settings()
+{
+    register_setting('wp_sp_settings_group', 'spCustomSettings');
+}
+
+add_action('admin_init', 'sp_custom_settings_settings');
